9edfb113e274772c3309bccd2fc9b7991b95f96d
Hanyuan Zhu <hzhu18@sva.edu>
2026-04-17 11:52:57 -0400

    Refactor canvas creation and improve draw logic

--- sketch.js
+++ sketch.js
@@ -8,7 +8,8 @@ let mic;
 let started = false;
 
 function setup() {
-  createCanvas(1280, 960, WEBGL);
+  let canvas = createCanvas(1280, 960, WEBGL);
+  canvas.parent('canvas-holder');
   pixelDensity(1);
   imageMode(CENTER);
 
@@ -39,19 +40,20 @@ function setup() {
     }
   });
 }
+
 function draw() {
   background(0);
-  if (!started) return; 
+  if (!started) return;
   scale(-1, 1);
   d = sliderOne.value();
-  let t = frameCount*0.02;
-  let level = mic.getLevel()
+  let t = frameCount * 0.02;
+  let level = mic.getLevel();
   levSm = lerp(levSm, level, 0.8);
   push();
-  if(levSm>0.01){
-    scale(map(abs(sin(t)),0,1,1,1.2));
+  if (levSm > 0.01) {
+    scale(map(abs(sin(t)), 0, 1, 1, 1.2));
   }
-  let rot = map(levSm, 0.001, 0.15, -PI/4, PI/4);
+  let rot = map(levSm, 0.001, 0.15, -PI / 4, PI / 4);
   cam.loadPixels();
   old.loadPixels();
   translate(-cam.width / 2, -cam.height / 2, 0);
@@ -60,45 +62,45 @@ function draw() {
   let threshold = 5;
 
   for (let y = 0; y < cam.height; y = y + l) {
-      for (let x = 0; x < cam.width; x = x + l) {
-          for (let z = 0; z < l * 10; z = z + l) {
-            let p = (x + y * cam.width) * 4;
-            let r = cam.pixels[p];
-            let g = cam.pixels[p + 1];
-            let b = cam.pixels[p + 2];
-            let br = (r * 2 + g * 3 + b) / 6;
-            let a = br * map(z, 0, 360, 2, 0.001) * map(levSm,0.001,0.1,0.001,2);
-            let oldR = old.pixels[p];
-            let oldG = old.pixels[p + 1];
-            let oldB = old.pixels[p + 2];
-            let oldBr = (oldR * 2 + oldG * 3 + oldB) / 6;
-            sliderOne.value(map(br, 0, 255, 2, 30));
-            let motion = abs(oldBr - br);
-            push();
-            translate(x + l/2, y + l/2, z + l/2)
-            rotateX(sin(0.01*(motion + x)));
-            rotateY(cos(0.01*(motion + y)));
-            cube(r, g, b, a);
-            pop();
-            }
-        }
+    for (let x = 0; x < cam.width; x = x + l) {
+      for (let z = 0; z < l * 10; z = z + l) {
+        let p = (x + y * cam.width) * 4;
+        let r = cam.pixels[p];
+        let g = cam.pixels[p + 1];
+        let b = cam.pixels[p + 2];
+        let br = (r * 2 + g * 3 + b) / 6;
+        let a = br * map(z, 0, 360, 2, 0.001) * map(levSm, 0.001, 0.1, 0.001, 2);
+        let oldR = old.pixels[p];
+        let oldG = old.pixels[p + 1];
+        let oldB = old.pixels[p + 2];
+        let oldBr = (oldR * 2 + oldG * 3 + oldB) / 6;
+        sliderOne.value(map(br, 0, 255, 2, 30));
+        let motion = abs(oldBr - br);
+        push();
+        translate(x + l / 2, y + l / 2, z + l / 2);
+        rotateX(sin(0.01 * (motion + x)));
+        rotateY(cos(0.01 * (motion + y)));
+        cube(r, g, b, a);
+        pop();
+      }
     }
+  }
   cam.updatePixels();
   pop();
 }
 
-function cube(r, g, b, a) {  
+function cube(r, g, b, a) {
   noStroke();
-  for(let i=0;i<6;i++){
-    if(i%2==0){
-      rotateX(PI/2);
-    }else{
-      rotateY(PI/2);
+  for (let i = 0; i < 6; i++) {
+    if (i % 2 == 0) {
+      rotateX(PI / 2);
+    } else {
+      rotateY(PI / 2);
     }
     push();
     fill(r, g, b, a);
-    translate(0,0,-d/2);
+    translate(0, 0, -d / 2);
     plane(d, d);
     pop();
-  } 
+  }
 }
